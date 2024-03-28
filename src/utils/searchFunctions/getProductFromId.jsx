51b603d9086c26aa56841eb60bde38d2061516ad
StrixWL <brahimstrix@gmail.com>
2024-03-28 11:32:25 +0000

    chore: Remove obsolete files

--- src/utils/searchFunctions/getProductFromId.jsx
+++
@@ -1,7 +0,0 @@
-import { getProductById } from "../../services/ProductService";
-
-const getProductFromId = async  (productId) => {
-  const product = await  getProductById(productId);
-  return product;
-};
-export default getProductFromId;
