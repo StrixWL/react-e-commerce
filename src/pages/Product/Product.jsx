cb5fd3e6d8651d89fea9301df79aa6705d19421f
StrixWL <brahimstrix@gmail.com>
2024-03-29 08:51:58 +0000

    feat(Product): Add "Buy it now" and "Add to cart" buttons and product description

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -3,12 +3,13 @@ import { useParams } from "react-router-dom";
 import { getProductById } from "../../services/products";
 import styles from "./Product.module.css";
 import QuantityInput from "../../components/ui/QuantityInput/QuantityInput";
+import WideButton from "../../components/ui/WideButton/WideButton";
 
 const Product = () => {
 	const params = useParams();
 	const [product, setProduct] = useState({
-        image: ""
-    });
+		image: "",
+	});
 	useEffect(() => {
 		getProductById(params.productID).then(setProduct);
 	}, []);
@@ -19,17 +20,27 @@ const Product = () => {
 		<div className={styles["wrapper"]}>
 			<div className={styles["product"]}>
 				<div className={styles["product-imgs"]}>
-                    <img src={product.image} />
-                </div>
+					<img src={product.image} />
+				</div>
 				<div className={styles["product-details-wrapper"]}>
-                    <div className={styles['product-details']}>
-                        <h1>{product.title}</h1>
-                        <p>$ {product.price}</p>
+					<div className={styles["product-details"]}>
+						<h1>{product.title}</h1>
+						<span>$ {product.price}</span>
 						<h2>Quantity</h2>
-                        <QuantityInput />
-						
-                    </div>
-                </div>
+						<QuantityInput />
+						<WideButton
+							className={styles["wide-button"]}
+							text="Add to cart"
+						/>
+						<WideButton
+							className={styles["wide-button"]}
+							bgColor="black"
+							textColor="white"
+							text="Buy it now"
+						/>
+						<p>{product.description}</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
