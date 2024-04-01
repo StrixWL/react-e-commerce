4f5bcfc0739af900ba720852309b9ca57d897a5e
StrixWL <brahimstrix@gmail.com>
2024-04-01 12:02:37 +0000

    feat(header): implement toggle button for cart

--- src/components/Header/UtilitySection/Utilities/Utilities.jsx
+++ src/components/Header/UtilitySection/Utilities/Utilities.jsx
@@ -6,26 +6,31 @@ import DropDownMenu from "../../../ui/DropDownMenu/DropDownMenu";
 import useLocation from "../../../../hooks/useLocation";
 import { locations } from "../../../../data/locations";
 import SearchButton from "../../../ui/SearchButton/SearchButton";
+import { CartContext } from "../../../../context/CartContext";
+import { useContext } from "react";
 
-const Utilities = ({setShowNav}) => {
+const Utilities = ({ setShowNav }) => {
 	const { currentLocation, changeLocation } = useLocation();
+	const { toggleCart } = useContext(CartContext);
 	return (
 		<div className={styles["utilities"]}>
 			<DropDownMenu
 				items={locations}
 				current={currentLocation}
 				onChange={changeLocation}
-				className={styles['dropdown-menu']}
+				className={styles["dropdown-menu"]}
 			/>
-			<SearchButton setShowNav={setShowNav} className={styles['search']} />
+			<SearchButton
+				setShowNav={setShowNav}
+				className={styles["search"]}
+			/>
 			<Link className={styles["login"]} to="/login">
 				<img src={loginIcon}></img>
 			</Link>
-			<div className={styles["cart"]} onClick={() => {}}>
+			<div className={styles["cart"]} onClick={toggleCart}>
 				<img src={cartIcon}></img>
-				<span className={styles['cart-count']}>{0}</span>
+				<span className={styles["cart-count"]}>{0}</span>
 			</div>
-		
 		</div>
 	);
 };
