fb953b86cb43502b2b6be9d3da2c1bda2b0c7333
StrixWL <brahimstrix@gmail.com>
2024-04-02 15:57:25 +0000

    feat(search): implement search by category functionality

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -3,19 +3,34 @@ import styles from './Search.module.css';
 import searchIcon from '../../assets/searchb.svg'
 import Products from '../../components/Products/Products';
 import { useState } from 'react';
+import DropDownMenu2 from '../../components/ui/DropDownMenu2/DropDownMenu2';
 
 const Search = () => {
     const [keyWords, setKeyWords] = useState(null)
-    const onSubmit = (e) => {
+    const [currentCategory, setCurrentCategory] = useState(null)
+    const onSearchSubmit = (e) => {
         e.preventDefault()
         setKeyWords(e.target[0].value)
+    }
+    const onCategoryChange = (e) => {
+        if (e.target.value == 'All') {
+            setCurrentCategory(null)
+            return
+        }
+        setCurrentCategory(e.target.value.toLowerCase())
     }
     return (
         <div className={styles['search']}>
             <div className={styles['input-wrapper']}>
-                <WideInput color="0, 0, 0" onSubmit={onSubmit} buttonIcon={searchIcon} className={styles['input']} placeholder="Search" required={false}/>
+                <WideInput color="0, 0, 0" onSubmit={onSearchSubmit} buttonIcon={searchIcon} className={styles['input']} placeholder="Search" required={false}/>
             </div>
-            <Products category={null} keyWords={keyWords}/>
+            <div className={styles['category-wrapper']}>
+                <div className={styles['category']}>
+                    <span>Category:</span>
+                    <DropDownMenu2 onChange={onCategoryChange} items={['All', 'Electronics', 'Men\'s clothing', 'Women\'s clothing']}/>
+                </div>
+            </div>
+            <Products category={currentCategory} keyWords={keyWords}/>
         </div>
     )
 }
