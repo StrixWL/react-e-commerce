5bea6309e3c15ae6dbdb063c96a5b716f515f7ce
StrixWL <brahimstrix@gmail.com>
2024-04-02 11:49:23 +0000

    feat(cart): add delete button and price (static, not implemented yet)

--- src/components/Cart/CartList/CartListElem/CartListElem.jsx
+++ src/components/Cart/CartList/CartListElem/CartListElem.jsx
@@ -1,5 +1,6 @@
 import QuantityInput from '../../../ui/QuantityInput/QuantityInput';
 import styles from './CartListElem.module.css';
+import deleteIcon from '../../../../assets/delete.svg'
 
 const CartListElem = ({item}) => {
     return (
@@ -9,8 +10,16 @@ const CartListElem = ({item}) => {
             </div>
             <div className={styles['product-details']}>
                 {/* <h2>White Gold Plated Princess</h2> */}
-                <h2>{item.title}</h2>
-                <QuantityInput className={styles['quantity-input']} />
+                <div className={styles['title-and-price']}>
+                    <h2>{item.title}</h2>
+                    <span>$112.00</span>
+                </div>
+                <div className={styles['quantity']}>
+                    <QuantityInput className={styles['quantity-input']} />
+                    <button className={styles['delete-btn']}>
+                        <img src={deleteIcon} />
+                    </button>
+                </div>
             </div>
         </li>
     )
