df6ee82980a4118bd3058458df90c1041f8ef85c
StrixWL <brahimstrix@gmail.com>
2024-04-02 15:56:42 +0000

    fix(cart): cart no longer active on page load

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const CartContext = createContext()
 
 export const CartProvider = ({children}) => {
-    const [showCart, setShowCart] = useState(true)
+    const [showCart, setShowCart] = useState(false)
     const toggleCart = () => setShowCart(!showCart)
     return (
         <CartContext.Provider value={{showCart, toggleCart}}>
