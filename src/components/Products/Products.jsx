08f4b6f129ae86f9d789ac1faf5daf80d4f6681d
StrixWL <brahimstrix@gmail.com>
2024-04-02 14:03:28 +0000

    feat(search): implement search by keywords functionality

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,7 +3,7 @@ import styles from "./Products.module.css";
 import Product from "./Product/Product";
 import { useEffect, useState } from "react";
 
-const Products = () => {
+const Products = ({category, keyWords}) => {
 	const [columnsCount, setColumnsCount] = useState(-1);
 	const [items, setItems] = useState([]);
 
@@ -12,16 +12,24 @@ const Products = () => {
 			setItems(await getProducts())
 		})()
 	}, [])
-
 	useEffect(() => {
 		if (window.innerWidth > 989) setColumnsCount(3);
 		else if (window.innerWidth > 749) setColumnsCount(2);
 		else setColumnsCount(1);
 	});
+	const filterItem = (item) => {
+		if (!category && !keyWords)
+			return true
+		if (!keyWords && category)
+			return (item.category == category)
+		if (!category && keyWords)
+			return (item.title.toLowerCase().includes(keyWords.toLowerCase()))
+		return ((item.category == category) && item.title.toLowerCase().includes(keyWords.toLowerCase()))
+	}
 	return (
 		<div className={styles["wrapper"]}>
 			<ul className={styles["items"]}>
-				{items.map((item, i) => {
+				{items.filter(filterItem).map((item, i) => {
 					return (
 						<li
 							data-aos-duration={700 + (i % columnsCount) * 400}
--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -2,14 +2,20 @@ import WideInput from '../../components/ui/WideInput/WideInput';
 import styles from './Search.module.css';
 import searchIcon from '../../assets/searchb.svg'
 import Products from '../../components/Products/Products';
+import { useState } from 'react';
 
 const Search = () => {
+    const [keyWords, setKeyWords] = useState(null)
+    const onSubmit = (e) => {
+        e.preventDefault()
+        setKeyWords(e.target[0].value)
+    }
     return (
         <div className={styles['search']}>
             <div className={styles['input-wrapper']}>
-                <WideInput color="0, 0, 0" onSubmit={() => {}} buttonIcon={searchIcon} className={styles['input']} placeholder="Search"/>
+                <WideInput color="0, 0, 0" onSubmit={onSubmit} buttonIcon={searchIcon} className={styles['input']} placeholder="Search" required={false}/>
             </div>
-            <Products />
+            <Products category={null} keyWords={keyWords}/>
         </div>
     )
 }
