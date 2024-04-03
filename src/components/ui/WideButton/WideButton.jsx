3818a7473776a08e93c6e01d140f64ae0d326717
StrixWL <brahimstrix@gmail.com>
2024-04-03 16:25:08 +0000

    feat(ui): add onclick property to WideButton component

--- src/components/ui/WideButton/WideButton.jsx
+++ src/components/ui/WideButton/WideButton.jsx
@@ -1,12 +1,13 @@
 import styles from "./WideButton.module.css";
 
-const WideButton = ({ className, borderColor, textColor, bgColor, text }) => {
+const WideButton = ({ className, borderColor, textColor, bgColor, text, onClick }) => {
 	className = className || "";
 	borderColor = borderColor || "black";
     bgColor = bgColor || "transparent"
     textColor = textColor || "black"
 	return (
 		<button
+		onClick={onClick}
 			style={{
 				"--border-color": borderColor,
 				"--text-color": textColor,
