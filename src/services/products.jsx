e23ea18c1c5b07f955839c6e1ddd6641e0f895ef
StrixWL <brahimstrix@gmail.com>
2024-04-03 16:26:39 +0000

    feat(products service): fetched data is now being memoized for reuse

--- src/services/products.jsx
+++ src/services/products.jsx
@@ -1,11 +1,15 @@
-export const getProducts = async () => {
-    const data = await fetch('https://fakestoreapi.com/products')
-        .then(res => res.json())
-    return data
-}
+const cache = {};
 
-export const getProductById = async (id) => {
-    const data = await fetch('https://fakestoreapi.com/products/' + id)
-        .then(res => res.json())
-    return data
-}
+const memFetch = async (url) => {
+	if (cache[url])
+        return cache[url];
+	const data = await fetch(url).then((res) =>
+		res.json()
+	);
+    cache[url] = data;
+	return data;
+};
+
+export const getProducts = async () => memFetch('https://fakestoreapi.com/products')
+
+export const getProductById = async (id) => memFetch("https://fakestoreapi.com/products/" + id)
