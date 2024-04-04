09fe234a04bb133e73c5950d6ecbc4ee6595e471
StrixWL <brahimstrix@gmail.com>
2024-04-04 10:10:31 +0000

    feat(cart context): set limit on how much items per product user can add to cart (1000)

--- src/context/Cart/CartItemsContext.jsx
+++ src/context/Cart/CartItemsContext.jsx
@@ -1,16 +1,20 @@
-import { createContext, useEffect, useMemo, useReducer } from "react";
+import { createContext, useReducer } from "react";
 
 export const CartItemsContext = createContext();
 
 export const CartItemsProvider = ({ children }) => {
 	const [cartItems, dispatchCart] = useReducer((state, action) => {
+		// whitespace to preserve order in our obj
         const id = action.id.toString() + ' '
 		switch (action.type) {
 			case "ADD":
+				let newAmount = action.amount + (state[id] ? state[id].amount : 0)
+				if (newAmount > 1000)
+					newAmount = 1000
 				return {
 					...state,
 					[id]: {
-                        amount:  action.amount + (state[id] ? state[id].amount : 0),
+                        amount:  newAmount,
                         details: action.details
                     },
 				};
@@ -22,6 +26,10 @@ export const CartItemsProvider = ({ children }) => {
                         details: action.details
                     },
 				};
+			case "DELETE":
+				const newState = {...state}
+				delete newState[id]
+				return newState
 		}
 	}, {});
 	return (
