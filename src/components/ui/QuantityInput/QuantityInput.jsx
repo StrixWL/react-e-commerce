7ade519a8f93b3a3e995a8df8ba98f8e5823706a
StrixWL <brahimstrix@gmail.com>
2024-04-04 10:05:07 +0000

    feat(ui): add min and max props to QuantityInput component

--- src/components/ui/QuantityInput/QuantityInput.jsx
+++ src/components/ui/QuantityInput/QuantityInput.jsx
@@ -1,25 +1,28 @@
 import { useEffect, useRef, useState } from 'react';
 import styles from './QuantityInput.module.css'
 
-const QuantityInput = ({setQuantity, className, defaultValue}) => {
+const QuantityInput = ({setQuantity, className, defaultValue, min, max}) => {
     const inputRef = useRef(null)
 
     className = className || ''
     defaultValue = defaultValue || 1
-    const increment = () => (inputRef.current && setQuantity(++inputRef.current.value))
-    const decrement = () => (inputRef.current && inputRef.current.value > 1 && setQuantity(--inputRef.current.value))
+    const increment = () => (inputRef.current && inputRef.current.value < max && setQuantity(++inputRef.current.value))
+    const decrement = () => (inputRef.current && inputRef.current.value > min && setQuantity(--inputRef.current.value))
+    max == undefined && (max = 1000)
+    min == undefined && (min = 1)
     const handleChange = () => {
         if (inputRef.current.value != '')
             inputRef.current.value = Math.floor(inputRef.current.value)
-        if (inputRef.current.value >= 1)
+        if (inputRef.current.value > max)
+            inputRef.current.value = max
+        if (inputRef.current.value >= min)
             setQuantity(Math.floor(inputRef.current.value))
     }
-    useEffect(() => {
-        console.log("QuantityInput")
-    })
     const handleBlur = () => {
-        if (inputRef.current.value < 1)
-            inputRef.current.value = 1
+        if (inputRef.current.value < min)
+            inputRef.current.value = min
+        if (inputRef.current.value > max)
+            inputRef.current.value = max
         inputRef.current.value = Math.floor(inputRef.current.value)
         setQuantity(Math.floor(inputRef.current.value))
     }
